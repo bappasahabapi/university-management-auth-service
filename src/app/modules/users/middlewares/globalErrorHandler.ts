45116593c9b4b3f10b9d5c21a434144030c2ad1d
Bappa Saha <bappasaha161@gmail.com>
2023-06-06 00:44:21 +0600

    d

--- src/app/modules/users/middlewares/globalErrorHandler.ts
+++ src/app/modules/users/middlewares/globalErrorHandler.ts
@@ -1,10 +1,27 @@
 import { NextFunction, Request, Response } from "express";
+import config from "../../../../config";
+import { IGenericErrorMessage } from "../../../../interfaces/error";
 
 const globalErrorHandler = (err, req: Request, res: Response, next: NextFunction) => {
     
-    res.status(400).json({
-        bapiErr:err
+    // res.status(400).json({bapiErr:err})
+    
+   
+
+    const  statusCode =5000;
+    const message ='Something went wrong';
+    const errorMessages:IGenericErrorMessage[] =[]
+
+
+
+
+    res.status(statusCode).json({
+        success:false,
+        message,
+        errorMessages,
+        stack: config.env !== 'production' ? err.stack : undefined
     })
+
     next();
 }
 
--- src/config/index.ts
+++ src/config/index.ts
@@ -6,6 +6,7 @@ dotenv.config({
 })
 
 export default {
+  env:process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   default_user_pass: process.env.DEFAULT_USER_PASS,
