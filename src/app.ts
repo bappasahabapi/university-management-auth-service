9e6bf8e43849f5943ca17499b8df5fee265038b2
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-06-09 21:48:39 +0600

    handle not found route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routes from './app/routes';
 const app: Application = express();
@@ -21,5 +22,19 @@ app.use('/api/v1/', routes);
 
 //global error handler
 app.use(globalErrorHandler);
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(httpStatus.NOT_FOUND).json({
+        success: false,
+        message: 'Not Found',
+        errorMessages: [{
+            path: req.originalUrl,
+            // path: '.',
+            message: 'API is not found'
+        }]
+    })
+    next()
+})
 
 export default app;
