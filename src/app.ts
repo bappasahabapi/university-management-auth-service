dd6496757ab5635b7cc472819275eb472f383899
Bappa Saha <bappasaha161@gmail.com>
2023-06-16 23:15:18 +0600

    s

--- src/app.ts
+++ src/app.ts
@@ -11,14 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use('/api/v1/users/', UserRoutes);
-// app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
 app.use('/api/v1/', routes);
 
-//Testing
-// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
-//   throw new Error('Testing Error logger')
-// })
+
 
 //global error handler
 app.use(globalErrorHandler);
