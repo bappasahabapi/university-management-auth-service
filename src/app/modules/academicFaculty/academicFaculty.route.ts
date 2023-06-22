b14c7ad6551d15d3dd8dc4a42d63b1df53a729d0
Bappa Saha <bappasaha161@gmail.com>
2023-06-22 12:04:07 +0600

    [done ] create part

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicFacultyController } from './academicFaculty.controller';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
 
 
 const router = express.Router();
 
 router.post(
     '/create-faculty',
+    validateRequest(AcademicFacultyValidation.createFacultyZodSchema),
     AcademicFacultyController.createFaculty
 )
 
