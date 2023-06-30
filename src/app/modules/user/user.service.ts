9d2e3f9ba4dca956f03e94a1c574e2537211608b
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-06-30 14:03:28 +0600

    create student done

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,15 +1,15 @@
+import httpStatus from 'http-status';
 import mongoose from 'mongoose';
 import config from '../../../config/index';
 import ApiError from '../../../errors/ApiError';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { IStudent } from '../student/student.interfaces';
+import { Student } from '../student/student.model';
 import { IUser } from './user.interface';
 import { User } from './user.model';
 import { generateStudentId } from './user.utils';
-import { Student } from '../student/student.model';
-import httpStatus from 'http-status';
 
-const createStudent = async (student: IStudent, user: IUser): Promise<IUser | null > => {
+const createStudent = async (student: IStudent, user: IUser): Promise<IUser | null> => {
 
   // default password
   if (!user.password) {
@@ -21,6 +21,7 @@ const createStudent = async (student: IStudent, user: IUser): Promise<IUser | nu
   const academicsemester = await AcademicSemester.findById(student.academicSemester);
 
   //generate student id
+  let newUserAllData = null;
   const session = await mongoose.startSession()
 
   try {
@@ -28,45 +29,57 @@ const createStudent = async (student: IStudent, user: IUser): Promise<IUser | nu
     session.startTransaction()
     const id: string | undefined = await generateStudentId(academicsemester) as string;
 
-    user.id =id
-    student.id =id
+    user.id = id
+    student.id = id
 
     //array
-    const newStudent =await Student.create([student],{session});
+    const newStudent = await Student.create([student], { session });
 
-    if(!createStudent.length){
-      throw new ApiError(httpStatus.BAD_REQUEST,'Failed to crete student')
+    if (!createStudent.length) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Failed to crete student')
     }
 
     //set student_id into user.student
-    user.student=newStudent[0]._id
-    const newUser =await User.create([user],{session});
+    user.student = newStudent[0]._id
 
-    if(!newUser.length){
-      throw new ApiError(httpStatus.BAD_REQUEST,'Failed to crete user')
+    const newUser = await User.create([user], { session });
+
+    if (!newUser.length) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Failed to crete user')
     }
+
+    newUserAllData = newUser[0];
 
     await session.commitTransaction();
     await session.endSession();
 
 
   } catch (error) {
-    await session.abortTransaction()
-    await session.endSession()
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
   }
 
-
-
-
-
-
-
-  const createdUser = await User.create(user);
-
-  if (!createdUser) {
-    throw new ApiError(400, 'Failed to create');
+  if (newUserAllData) {
+    newUserAllData = await User.findOne({ id: newUserAllData.id }).populate({
+      path: 'student',
+      populate: [
+        {
+          path: 'academicSemester'
+        },
+        {
+          path: 'academicDepartment'
+        },
+        {
+          path: 'academicFaculty'
+        }
+      ]
+    })
   }
-  return createdUser;
+
+  return newUserAllData
+
+
 };
 
 export const UserService = {
